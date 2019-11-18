72981532fa3c36d31baec4e22a63c5ae61253ea7
liana <hilllia@ssfs.org>
2019-11-18 11:19:39 -0500

    struggled with working on unit 8 daily exercises

--- daily_exercises.py
+++ daily_exercises.py
@@ -7,14 +7,24 @@ from tkinter import *
 
 root = Tk()
 
-# problem one
-hello_label = Label(root, text="Hello World")
-hello_label.grid(row=1, column=1)
+# # problem one
+# hello_label = Label(root, text="Hello World")
+# hello_label.grid(row=1, column=1)
 
 
 # problem two
 user_name = StringVar()
-user_name.grid(row=2, column=1)
+user_name = Entry(root, textvariable=user_name)
+user_name.grid(row=1, column=1)
+
+
+hello = Label(root)
+hello.grid(row=2, column=1)
+say_hello = Button(root, text="Say Hello")
+say_hello.grid(row=3, column=1)
+greeting = Label(root, text="Hello", )
+greeting.grid(row=2, column=1)
+
 
 root.mainloop()
 
--- dice.py
+++ dice.py
@@ -1,7 +1,17 @@
 from tkinter import *
+import random
+
+
+def roll_dice():
+    max = side_choice.get()
+    roll = random.randint(1, max)
+    roll_value.set(roll)
+
 
 root = Tk()
 side_choice = IntVar()
+roll_value = IntVar()
+
 title_label = Label(root, text="Dice Roller")
 title_label.grid(row=1, column=1)
 
@@ -20,10 +30,9 @@ ten_sided.grid(row=5, column=1)
 twenty_sided = Radiobutton(root, text="20-sided", variable=side_choice, value=20)
 twenty_sided.grid(row=6, column=1)
 
-roll_button = Button(root, text="Roll Dice")
+roll_button = Button(root, text="Roll Die", command=roll_dice)
 roll_button.grid(row=7, column=1)
 
-roll_result = Label(root, text="0")
-roll_button.grid(row=8, column=1)
-
+roll_result = Label(root, textvariable=roll_value)
+roll_result.grid(row=8, column=1)
 root.mainloop()
